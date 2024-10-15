9f4e6162b585e713750ff96bb7769cef8620ee7f
msdsn <devsanms@outlook.com>
2024-10-15 14:59:41 +0300

    Refactor main2.py and base_model2.py: Remove unused imports and clean up code

---
+++ opengait/modeling/base_model2.py
@@ -0,0 +1,241 @@
+"""The base model definition.
+
+This module defines the abstract meta model class and base model class. In the base model,
+ we define the basic model functions, like get_loader, build_network, and run_train, etc.
+ The api of the base model is run_train and run_test, they are used in `opengait/main.py`.
+
+Typical usage:
+
+BaseModel.run_train(model)
+BaseModel.run_test(model)
+"""
+import torch
+import numpy as np
+import os.path as osp
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as tordata
+
+from tqdm import tqdm
+from torch.cuda.amp import autocast
+from torch.cuda.amp import GradScaler
+from abc import ABCMeta
+from abc import abstractmethod
+
+from . import backbones
+from .loss_aggregator import LossAggregator
+from data.transform import get_transform
+from data.collate_fn import CollateFn
+from data.dataset import DataSet
+import data.sampler as Samplers
+from utils import Odict, mkdir, ddp_all_gather
+from utils import get_valid_args, is_list, is_dict, np2var, ts2np, list2var, get_attr_from
+from evaluation import evaluator as eval_functions
+from utils import NoOp
+from utils import get_msg_mgr
+
+__all__ = ['BaseModel']
+
+
+class MetaModel(metaclass=ABCMeta):
+    """The necessary functions for the base model.
+
+    This class defines the necessary functions for the base model, in the base model, we have implemented them.
+    """
+    @abstractmethod
+    def get_loader(self, data_cfg):
+        """Based on the given data_cfg, we get the data loader."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def build_network(self, model_cfg):
+        """Build your network here."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def init_parameters(self):
+        """Initialize the parameters of your network."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_optimizer(self, optimizer_cfg):
+        """Based on the given optimizer_cfg, we get the optimizer."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_scheduler(self, scheduler_cfg):
+        """Based on the given scheduler_cfg, we get the scheduler."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_ckpt(self, iteration):
+        """Save the checkpoint, including model parameter, optimizer and scheduler."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def resume_ckpt(self, restore_hint):
+        """Resume the model from the checkpoint, including model parameter, optimizer and scheduler."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def inputs_pretreament(self, inputs):
+        """Transform the input data based on transform setting."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def train_step(self, loss_num) -> bool:
+        """Do one training step."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def inference(self):
+        """Do inference (calculate features.)."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def run_train(model):
+        """Run a whole train schedule."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def run_test(model):
+        """Run a whole test schedule."""
+        raise NotImplementedError
+
+
+class BaseModel(MetaModel, nn.Module):
+    """Base model.
+
+    This class inherites the MetaModel class, and implements the basic model functions, like get_loader, build_network, etc.
+
+    Attributes:
+        msg_mgr: the massage manager.
+        cfgs: the configs.
+        iteration: the current iteration of the model.
+        engine_cfg: the configs of the engine(train or test).
+        save_path: the path to save the checkpoints.
+
+    """
+
+    def __init__(self, cfgs, training):
+        """Initialize the base model.
+
+        Complete the model initialization, including the data loader, the network, the optimizer, the scheduler, the loss.
+
+        Args:
+        cfgs:
+            All of the configs.
+        training:
+            Whether the model is in training mode.
+        """
+
+        super(BaseModel, self).__init__()
+        self.msg_mgr = get_msg_mgr()
+        self.cfgs = cfgs
+        self.iteration = 0
+        self.engine_cfg = cfgs['trainer_cfg'] if training else cfgs['evaluator_cfg']
+        
+        self.train_loader = self.get_loader(cfgs['data_cfg'], train=True)
+        
+
+        self.device = torch.distributed.get_rank()
+        torch.cuda.set_device(self.device)
+        self.to(device=torch.device(
+            "cuda", self.device))
+
+        
+
+    def get_backbone(self, backbone_cfg):
+        pass
+
+    def build_network(self, model_cfg):
+        pass
+
+    def init_parameters(self):
+        pass
+
+    def get_loader(self, data_cfg, train=True):
+        sampler_cfg = self.cfgs['trainer_cfg']['sampler'] if train else self.cfgs['evaluator_cfg']['sampler']
+        print("sampler_cfg calismak uzere")
+        print(sampler_cfg)
+        print("data_cfg calismak uzere")
+        print(data_cfg)
+        dataset = DataSet(data_cfg, train)
+
+        Sampler = get_attr_from([Samplers], sampler_cfg['type'])
+        vaild_args = get_valid_args(Sampler, sampler_cfg, free_keys=[
+            'sample_type', 'type'])
+        sampler = Sampler(dataset, **vaild_args)
+
+        loader = tordata.DataLoader(
+            dataset=dataset,
+            batch_sampler=sampler,
+            collate_fn=CollateFn(dataset.label_set, sampler_cfg),
+            num_workers=data_cfg['num_workers'])
+        return loader
+
+    def get_optimizer(self, optimizer_cfg):
+        pass
+
+    def get_scheduler(self, scheduler_cfg):
+        pass
+
+    def save_ckpt(self, iteration):
+        pass
+    def _load_ckpt(self, save_name):
+        pass
+    def resume_ckpt(self, restore_hint):
+        pass
+    def fix_BN(self):
+        pass
+    def inputs_pretreament(self, inputs):
+        """Conduct transforms on input data.
+
+        Args:
+            inputs: the input data.
+        Returns:
+            tuple: training data including inputs, labels, and some meta data.
+        """
+        seqs_batch, labs_batch, typs_batch, vies_batch, seqL_batch = inputs
+        seq_trfs = self.trainer_trfs if self.training else self.evaluator_trfs
+        if len(seqs_batch) != len(seq_trfs):
+            raise ValueError(
+                "The number of types of input data and transform should be same. But got {} and {}".format(len(seqs_batch), len(seq_trfs)))
+        requires_grad = bool(self.training)
+        seqs = [np2var(np.asarray([trf(fra) for fra in seq]), requires_grad=requires_grad).float()
+                for trf, seq in zip(seq_trfs, seqs_batch)]
+
+        typs = typs_batch
+        vies = vies_batch
+
+        labs = list2var(labs_batch).long()
+
+        if seqL_batch is not None:
+            seqL_batch = np2var(seqL_batch).int()
+        seqL = seqL_batch
+
+        if seqL is not None:
+            seqL_sum = int(seqL.sum().data.cpu().numpy())
+            ipts = [_[:, :seqL_sum] for _ in seqs]
+        else:
+            ipts = seqs
+        del seqs
+        return ipts, labs, typs, vies, seqL
+
+    def train_step(self, loss_sum) -> bool:
+        pass
+
+    def inference(self, rank):
+        pass
+
+    @ staticmethod
+    def run_train(model):
+        """Accept the instance object(model) here, and then run the train loop."""
+        print("train loader calismak uzere")
+        for inputs in model.train_loader:
+            print('first')
+            break
+
+    @ staticmethod
+    def run_test(model):
+        pass
